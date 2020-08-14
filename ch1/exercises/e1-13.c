c0dab0c5597bc30261ad106a2aebf2131bcf5c22
Goran V <32798413+goranvel@users.noreply.github.com>
2020-08-13 21:27:38 -0500

    Update e1-13.c

--- ch1/exercises/e1-13.c
+++ ch1/exercises/e1-13.c
@@ -1,50 +1,48 @@
-#include <stdio.h>
-
 /* prints histogram of word lenghts */
-main() {
-	char c = 0;
-	int lenghts[30];	// array hoding lenghts of words
-	int wordLen = 0;	// current word lenght
-	int lonest = 0;	// longest word lenght
-	int maxCnt = 0;	// most frequent word lenght
+int main() {
+   int c = 0;
+   int lenghts[30];  // array hoding lenghts of words
+   int wordLen = 0;  // current word lenght
+   int longest = 0;  // longest word lenght
+   int maxCnt = 0;   // most frequent word lenght
 
-	// sets word counts to 0
-	int i = 0;
-	for(i = 0; i < 30; ++i) {
-		lenghts[i] = 0;
-	}
+   // sets word counts to 0
+   int i = 0;
+   for(i = 0; i < 30; ++i)
+      lenghts[i] = 0;
 
-	// counts number of letters in word
-	while((c = getchar()) != EOF) {
-		if(c == ' ' || c == '\t' || c == '\n') { // checks for end of word
-			lenghts[wordLen]++;
-			if(lenghts[wordLen] > maxCnt)	// checks for most frequent lenght
-				maxCnt = lenghts[wordLen];
-			wordLen = 0;
-		}  else {		// counts length of current word
-			wordLen++;
-		}
-		if(longest < wordLen) // gets maximum word lenght
-			longest = wordLen;
-	}
+   // counts number of letters in word
+   while((c = getchar()) != EOF) {
+      if(c == ' ' || c == '\t' || c == '\n') { // checks for end of word
+         lenghts[wordLen]++;
+         if(lenghts[wordLen] > maxCnt) // checks for most frequent lenght
+            maxCnt = lenghts[wordLen];
+         wordLen = 0;
+      }  else {      // counts length of current word
+         wordLen++;
+      }
+      if(longest < wordLen) // gets maximum word lenght
+         longest = wordLen;
+   }
 
-	// prints histogram data verticaly row by row
-	for(; maxCnt > 0; --maxCnt) {	// maxCnt is used as Y axis value in our histogram
-		printf("%d", maxCnt); // prints numbers to the right
-		for(i = 1; i <= maxLen; ++i) { // prints dashes or spaces
-			if(words[i] >= maxCnt){	// prints - if word lenght is greater or equal to Y axis value
-				printf(" - ");
-			} else {	// prints SPACE if word lenght is smaller than Y accis value
-				printf("   ");
-			}
-		}
-		printf("\n");
-	}
+   // prints histogram data verticaly row by row
+   for(; maxCnt > 0; --maxCnt) { // maxCnt is used as Y axis value in our histogram
+      printf("%d", maxCnt); // prints numbers to the right
+      for(i = 0; i <= longest; ++i)  // prints dashes or spaces
+         if(lenghts[i] >= maxCnt)   // prints - if word lenght is greater or equal to Y axis value
+            printf("- ");
+         else // prints SPACE if word lenght is smaller than Y accis value
+            printf("  ");
 
-	// prints letter count on X axis
-	printf(" ");
-	for(i = 1; i <= maxLen; ++i){
-		printf(" %d ", i);
-	}
-	printf("\n");
+      printf("\n");
+   }
+
+   // prints letter count on X axis
+   printf("  ");
+   for(i = 0; i < longest; ++i){
+      printf(" %d",i);
+   }
+   printf("\n");
+
+   return 0;
 }
